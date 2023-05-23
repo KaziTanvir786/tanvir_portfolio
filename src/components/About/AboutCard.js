2725cf673c10efdae1022a5101525289aa10618f
KaziTanvir786 <kazitanvirakter@gmail.com>
2023-05-24 02:35:01 +0600

    Text editted

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-import { ImPointRight } from "react-icons/im";
+import { TfiQuoteLeft, TfiQuoteRight } from "react-icons/tfi";
+import { TiPointOfInterest } from "react-icons/ti";
 
 function AboutCard() {
   return (
@@ -8,30 +9,35 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
-            <br />I am a junior pursuing IMSC in Maths and Computing in BIT
-            Mesra.
+            Hi Everyone, I am <span className="purple">Kazi Tanvir Akter </span>
+            from <span className="purple"> Dhaka, Bangladesh.</span>
+            <br />I am a CSE graduate from North South University, pursuing my higher studies in abroad.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <TiPointOfInterest /> Playing Games
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <TiPointOfInterest /> Writing Tech Blogs
             </li>
             <li className="about-activity">
-              <ImPointRight /> Travelling
+              <TiPointOfInterest /> Playing Guitar
+            </li>
+            <li className="about-activity">
+              <TiPointOfInterest /> Drawing Sketches
+            </li>
+            <li className="about-activity">
+              <TiPointOfInterest /> Travelling
             </li>
           </ul>
 
-          <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
+          <p style={{ color: "#1a85ff" }}>
+            <b>"Technology is the conversion of our imagination into reality!"</b>
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer"><b>Kazi Tanvir Akter</b></footer>
         </blockquote>
       </Card.Body>
     </Card>
